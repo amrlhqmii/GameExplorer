377195854a2e8b7fd6094f0fdbeace75fb306e68
MUHAMMAD AMIRUL HAQIMI BIN MOHD ZAMIR <92685367+amrlhqmii@users.noreply.github.com>
2022-01-26 02:37:10 +0800

    Update ui.R
    Added User's guide

--- IMDBMovieExplorer/ui.R
+++ IMDBMovieExplorer/ui.R
@@ -36,23 +36,48 @@ fluidPage(
       ),
       wellPanel(
         h4("Powered by:"),
-        tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/RStudio-Ball.png", height = 50, length = 50),
+        tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/RStudio-Ball.png", height = 70, length = 70),
       ),
     ),
-    column(9,
-      ggvisOutput("plot1"),
-      wellPanel(
-        span("Number of movies selected:",
-          textOutput("n_movies")
-        ),
-      ),
-      wellPanel(
-        h4("Oscars"),
-        tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/Oscars.jpg", height = 300, length = 300),
-        h5("The Academy Awards, popularly known as the Oscars, 
+    mainPanel(
+      tabsetPanel(type = "tab",
+                  tabPanel("Data",column(6,
+                                         ggvisOutput("plot1"),
+                                         wellPanel(
+                                           span("Number of movies selected:",
+                                                textOutput("n_movies")
+                                           ),
+                                         ),
+                                         wellPanel(
+                                           h4("Oscars"),
+                                           tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/Oscars.jpg", height = 300, length = 300),
+                                           h5("The Academy Awards, popularly known as the Oscars, 
           are awards for artistic and technical merit in the film industry. 
           They are regarded by many as the most prestigious and significant 
           awards in the entertainment industry worldwide."),
-      ),)
+                                         ),)),
+                  tabPanel("User Guide",column(12,
+                                               wellPanel(
+                                                 h3("How To Use?"),
+                                                 h5("1. Choose any movie's genre based on your interest (a movie can have multiple genres)."
+                                                    ),
+                                                 tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/Screenshot%202022-01-26%20014135.png", height = 200, length = 200),
+                                                 h5("2. Choose the range of any year you want the movie to be released and the range of 
+                                                 how much moneys its made from Box Office (millions)."),
+                                                 tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/Screenshot%202022-01-26%20014151.png", height = 200, length = 200),
+                                                 h5("3. Choose how much the minimum Oscars you want the movies won for all categories."),
+                                                 tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/Screenshot%202022-01-26%20014207.png", height = 200, length = 200),
+                                                 h5("4. Two optional filters are given to let you choose any movie contain any cast or you want any movie to 
+                                                    be direct by any director chosen. You can let it empty if you want."),
+                                                 tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/Screenshot%202022-01-26%20014222.png", height = 200, length = 200),
+                                                 h5("5. User can choose the graph's horizontal and vertical variable themselves by choose it from the list."),
+                                                 tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/Screenshot%202022-01-26%20014054.png", height = 200, length = 200),
+                                                 h5("6. Every single movie that met all the criteria chosen will shows up on the graph (Year released = 1970-2014,
+                                                    Min Oscars won = 1, Moneys made from Box Office(millions) = 200-800). (e.g King Kong(2005))."),
+                                                 tags$img(src = "https://raw.githubusercontent.com/amrlhqmii/IMDBMovieExplorer/main/image/Screenshot%202022-01-26%20015317.png", height = 400, length = 400),
+                                               )))
+                  ),
+    ),
   )
 )
+
